680cc14855869ebe1708d0c4210bacdee14437b3
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-09 15:49:25 +0100

    plek van ingevoerde letter vinden en aanwijzen

--- Galgje.java
+++ Galgje.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Galgje {
 	public static String teRadenWoord = "Water"; //static = class variable, niet static = instance variable
+	public static StringBuilder sb = new StringBuilder("*****");
 	public static void main(String[] args) {
 		System.out.println("Welkom bij galgje!");
 		System.out.println("Het te raden woord: ");
@@ -14,26 +15,51 @@ public class Galgje {
 	    for(int i = 0; i < aantalBeurten; i++) {
 	    	String str = sc.nextLine();
 	    	checkenOfLetterBestaat(str);
-	    	
-	    	/*
-		    if(teRadenWoord.contains(str)) {
-		    	System.out.println("Yes, deze letter zit erin!");
-		    } else {
-		    	System.out.println("Helaas, probeer het nog eens..");
-		    }
-		    
-		    System.out.println("Het spel is klaar.");
-			*/
 	    }
 	}
-	    public static void checkenOfLetterBestaat(String str) {
-	    	if(teRadenWoord.contains(str)) {
-		    	System.out.println("Yes, deze letter zit erin!");
-		    } else {
-		    	System.out.println("Helaas, probeer het nog eens..");
-		    }
+	
+	public static void checkenOfLetterBestaat(String str) {
+		int positie = teRadenWoord.indexOf(str);
+	    if(teRadenWoord.contains(str)) {
+			System.out.println("Yes, deze letter zit erin! " + teRadenWoord.indexOf(str));
+			} else {
+		   	System.out.println("Helaas, probeer het nog eens..");
+		}
 		    
 		    System.out.println("Het spel is klaar.");
+		    sb.replace(positie, positie+1, str);
+		    System.out.println(sb);
+	}
+	
+	
+	    
+	    
+	    
+	    /*
+	    public static void woordPrinten(String hetWoord, char[] ingevoerdeLetters) {
+	    	//boolean woordGeprint = false;
+	    	
+	    	for(int a = 0; a < hetWoord.length(); a++) {
+	    		
+	    	}
 	    }
-
+		*/
 }
+
+
+/*
+public static boolean printWord(String word, char[] enteredLetters) {
+    // Iterate through all letters in word
+    boolean asteriskPrinted = false;
+    for (int i = 0; i < word.length(); i++) {
+        char letter = word.charAt(i);
+        // Check if letter already have been entered bu user before
+        if (inEnteredLetters(letter, enteredLetters))
+            System.out.print(letter); // If yes - print it
+        else {
+            System.out.print('*');
+            asteriskPrinted = true;
+        }
+    }
+
+*/
