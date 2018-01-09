57d2c67a0041546d87621b8c09faaed695d6ebf2
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-09 14:38:01 +0100

    Code met field variable

--- Galgje.java
+++ Galgje.java
@@ -2,21 +2,20 @@
 import java.util.Scanner;
 
 public class Galgje {
-
+	public static String teRadenWoord = "Water"; //static = class variable, niet static = instance variable
 	public static void main(String[] args) {
 		System.out.println("Welkom bij galgje!");
 		System.out.println("Het te raden woord: ");
-
-		String teRadenWoord = "Boot";
+		
 		int aantalBeurten = 20;
 		
 		System.out.println(teRadenWoord);
 		Scanner sc = new Scanner(System.in);
-	    
 	    for(int i = 0; i < aantalBeurten; i++) {
-	    	
 	    	String str = sc.nextLine();
-	    
+	    	checkenOfLetterBestaat(str);
+	    	
+	    	/*
 		    if(teRadenWoord.contains(str)) {
 		    	System.out.println("Yes, deze letter zit erin!");
 		    } else {
@@ -24,9 +23,17 @@ public class Galgje {
 		    }
 		    
 		    System.out.println("Het spel is klaar.");
-
+			*/
 	    }
-	     
 	}
+	    public static void checkenOfLetterBestaat(String str) {
+	    	if(teRadenWoord.contains(str)) {
+		    	System.out.println("Yes, deze letter zit erin!");
+		    } else {
+		    	System.out.println("Helaas, probeer het nog eens..");
+		    }
+		    
+		    System.out.println("Het spel is klaar.");
+	    }
 
 }
