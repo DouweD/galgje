f7a22293e02325624752f71737dd4a5bd9c6bcb9
Douwe Dijkink <douwedijkink@gmail.com>
2018-01-09 13:59:55 +0100

    Woorden raden werkt

--- Galgje.java
+++ Galgje.java
@@ -1,13 +1,26 @@
 
+import java.util.Scanner;
+
 public class Galgje {
 
 	public static void main(String[] args) {
-		System.out.println("Welkom bij Galgje!");
+		System.out.println("Welkom bij galgje!");
 		System.out.println("Het te raden woord: ");
 
 		String teRadenWoord = "Boot";
 		
 		System.out.println(teRadenWoord);
+		Scanner sc = new Scanner(System.in);
+	    String str = sc.nextLine();
+	    System.out.println(str);	    
+	    
+	    if(teRadenWoord.contains(str)) {
+	    	System.out.println("ja");
+	    } else {
+	    	System.out.println("nee");
+	    }
+	    
+	    System.out.println("Het spel is klaar.");
 	}
 
 }
